38b29b1937803d2e7827b5ec7afa23389f63d4e0
Plamen Doychev <125978729+doycheff@users.noreply.github.com>
2024-07-24 15:29:45 +0300

    Add latest products

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,18 +1,32 @@
-import { Link } from "react-router-dom";
-import styles from "./Home.module.css";
+import { useEffect, useState } from "react";
+import productsAPI from "../../api/products-api";
+import LatestProduct from "./latest-product/LatestProduct";
 
 export default function Home() {
+    const [latestProducts, setLatestProducts] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            // TODO: modify to fetch only latest products
+            const result = await productsAPI.getAll();
+
+            setLatestProducts(result.reverse().slice(0, 3));
+        })()
+    }, []);
+
     return (
-        <div className={styles.homePage}>
-            <div className={styles.container}>
-                <div className={styles.welcomeMessage}>
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900">
-                        TechHaven
-                    </h2>
-                    <div className={styles.buttonContainer}>
-                        <Link to={'/products'} className={styles.productsButton}>View all products</Link>
+        <div >
+            <div className="container mx-auto px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+                <h2 className="text-4xl font-bold tracking-tight text-gray-900 text-center mb-8">
+                    {latestProducts.length > 0 ? "Latest Products" : "No products yet.."}
+                </h2>
+                {latestProducts.length > 0 && (
+                    <div className="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:gap-8">
+                        {latestProducts.map(product => (
+                            <LatestProduct key={product._id} {...product} />
+                        ))}
                     </div>
-                </div>
+                )}
             </div>
         </div>
     );
---
+++ client/src/components/home/latest-product/LatestProduct.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+
+export default function LatestProduct({
+    _id,
+    name,
+    price,
+    image
+}) {
+    return (
+        <div className="group relative bg-white rounded-lg shadow-md overflow-hidden">
+            <Link to={`/products/${_id}/details`} >
+                <div className="aspect-w-1 aspect-h-1 overflow-hidden lg:h-80">
+                    <img
+                        src={image}
+                        alt={name}
+                        className="object-cover object-center w-full h-full"
+                        style={{ backgroundColor: 'transparent' }} // Ensure transparency
+                    />
+                </div>
+            </Link>
+            <div className="p-4">
+                <div className="flex justify-between">
+                    <h3 className="text-lg font-medium text-gray-900">
+                        {name}
+                    </h3>
+                    <p className="text-lg font-medium text-gray-900">${price}</p>
+                </div>
+                <div className="mt-4 flex justify-center">
+                    <Link to={`/products/${_id}/details`}>
+                        <button
+                            type="button"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                        >
+                            Details
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
