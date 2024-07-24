7a992623c51a62ebc4a697448d4dda64d0c5fa9c
Plamen Doychev <125978729+doycheff@users.noreply.github.com>
2024-07-24 11:17:48 +0300

    Add page routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Register from "./components/register/Register";
+import Products from "./components/products/Products";
+import ProductCreate from "./components/products/product-create/ProductCreate";
+import ProductDetails from "./components/products/product-details/ProductDetails";
 
 function App() {
 
@@ -7,7 +14,17 @@ function App() {
         <>
             <Header />
 
-            <Home />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+
+                <Route path='/products' element={<Products />} />
+                <Route path='/product/details' element={<ProductDetails />} />
+
+                <Route path='/create' element={<ProductCreate />} />
+
+            </Routes>
         </>
     )
 }
