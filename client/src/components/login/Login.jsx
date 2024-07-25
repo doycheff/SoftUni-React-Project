13363165b34edf5f979aa7f849c0e09d08bbbc8b
Plamen Doychev <125978729+doycheff@users.noreply.github.com>
2024-07-25 12:39:44 +0300

    Implement login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,8 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useLogin } from "../../hooks/useAuth";
+import { useForm } from "../../hooks/useForm";
+
 import logo from "../../assets/logo.jpg"
 
-
 export default function Login() {
+    const login = useLogin();
+    const navigate = useNavigate();
+    const { values, changeHandler, submitHandler } = useForm(
+        { email: '', password: '' },
+        async ({ email, password }) => {
+            try {
+                await login(email, password)
+                navigate('/')
+            } catch (err) {
+                console.log(err.message);
+            }
+        }
+    );
+
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -18,7 +35,7 @@ export default function Login() {
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form action="#" method="POST" className="space-y-6">
+                    <form onSubmit={submitHandler} className="space-y-6">
                         <div>
                             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                                 Email address
@@ -28,7 +45,8 @@ export default function Login() {
                                     id="email"
                                     name="email"
                                     type="email"
-                                    required
+                                    value={values.email}
+                                    onChange={changeHandler}
                                     autoComplete="email"
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
@@ -40,14 +58,15 @@ export default function Login() {
                                 <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                                     Password
                                 </label>
-                                
+
                             </div>
                             <div className="mt-2">
                                 <input
                                     id="password"
                                     name="password"
                                     type="password"
-                                    required
+                                    value={values.password}
+                                    onChange={changeHandler}
                                     autoComplete="current-password"
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 />
