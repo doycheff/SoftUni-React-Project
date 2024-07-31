e0724440299ef7be419c61d8eee0d0c3d588a872
Plamen Doychev <125978729+doycheff@users.noreply.github.com>
2024-07-31 14:08:55 +0300

    Add profile dropdown menu

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,3 +1,5 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+
 import logo from "../../assets/logo.jpg";
 
 import { Link, useLocation } from "react-router-dom";
@@ -10,21 +12,17 @@ function classNames(...classes) {
 
 export default function Header() {
     const location = useLocation();
-    const { isAuthenticated, email } = useAuthContext();
+    const { isAuthenticated, email, logout } = useAuthContext();
 
     const navigation = isAuthenticated
         ? [
             { name: 'Home', href: '/', current: false },
             { name: 'Products', href: '/products', current: false },
             { name: 'Create an item', href: '/products/create', current: false },
-            { name: 'My Profile', href: '/myProfile', current: false },
-            { name: 'Logout', href: '/logout', current: false },
         ]
         : [
             { name: 'Home', href: '/', current: false },
             { name: 'Products', href: '/products', current: false },
-            { name: 'Login', href: '/login', current: false },
-            { name: 'Register', href: '/register', current: false },
         ];
 
     return (
@@ -57,11 +55,54 @@ export default function Header() {
                         })}
                     </div>
                 </nav>
-                {isAuthenticated && (
-                    <div className="text-gray-300 text-lg font-medium">
-                        {email}
+                <Menu as="div" className="relative ml-3">
+                    <div>
+                        <MenuButton className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                            <span className="absolute -inset-1.5" />
+                            <span className="sr-only">Open user menu</span>
+                            <img
+                                src="https://cdn-icons-png.flaticon.com/512/21/21104.png"
+                                className="h-8 w-8 rounded-full"
+                            />
+                        </MenuButton>
                     </div>
-                )}
+
+                    <MenuItems
+                        transition
+                        className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+                    >
+                        {isAuthenticated ? (
+                            <div className='text-center'>
+                                <span className="text-black-300 text-md font-medium">
+                                    {email}
+                                </span>
+                                <MenuItem>
+                                    <Link to="/myProfile" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                        My Profile
+                                    </Link>
+                                </MenuItem>
+                                <MenuItem>
+                                    <Link to="/logout" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                        Logout
+                                    </Link>
+                                </MenuItem>
+                            </div>
+                        ) :
+                            <div className='text-center'>
+                                <MenuItem>
+                                    <Link to="/login" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                        Login
+                                    </Link>
+                                </MenuItem>
+                                <MenuItem>
+                                    <Link to="/Register" className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                        Register
+                                    </Link>
+                                </MenuItem>
+                            </div>
+                        }
+                    </MenuItems>
+                </Menu>
             </div>
         </header>
     );
