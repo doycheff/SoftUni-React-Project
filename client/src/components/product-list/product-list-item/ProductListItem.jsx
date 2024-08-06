e859c2ed04536b9ca50a32d05dafcec90015a72f
Plamen Doychev <125978729+doycheff@users.noreply.github.com>
2024-08-06 12:39:19 +0300

    Redesign ProductListItem

--- client/src/components/product-list/product-list-item/ProductListItem.jsx
+++ client/src/components/product-list/product-list-item/ProductListItem.jsx
@@ -7,34 +7,34 @@ export default function ProductListItem({
     image
 }) {
     return (
-        <div className="group relative">
+        <div className="group relative bg-white rounded-lg shadow-md overflow-hidden">
             <Link to={`/products/${_id}/details`} >
-                <div className="aspect-w-1 aspect-h-1 rounded-md overflow-hidden lg:h-80">
+                <div className="aspect-w-1 aspect-h-1 overflow-hidden lg:h-80">
                     <img
                         src={image}
+                        alt={name}
                         className="object-cover object-center w-full h-full"
+                        style={{ backgroundColor: 'transparent' }} 
                     />
                 </div>
             </Link>
-
-            <div className="mt-4 flex justify-between">
-                <div>
+            <div className="p-4">
+                <div className="flex justify-between">
                     <h3 className="text-lg font-medium text-gray-900">
                         {name}
                     </h3>
+                    <p className="text-lg font-medium text-gray-900">${price}</p>
                 </div>
-                <p className="text-lg font-medium text-gray-900">${price}</p>
-            </div>
-
-            <div className="mt-4 flex justify-center">
-                <Link to={`/products/${_id}/details`}>
-                    <button
-                        type="button"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                        Details
-                    </button>
-                </Link>
+                <div className="mt-4 flex justify-center">
+                    <Link to={`/products/${_id}/details`}>
+                        <button
+                            type="button"
+                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+                        >
+                            Details
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
     );
